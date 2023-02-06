db6f9e4de45168855ef0379d32f948d8aebc2a20
armagan-akyrt <akyurta@mef.edu.tr>
2023-02-06 21:01:20 +0300

    tilemaps,scripts
    started tilemaps & wrote control scripts for player & camera

---
+++ TopDownRPG/Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+
+    public Transform lookAt;
+    public float boundX = .15f;
+    public float boundY = .05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero; // frame differeces.
+
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+            delta.x = (transform.position.x < lookAt.position.x) ? deltaX - boundX : deltaX + boundX;
+
+
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+            delta.y = (transform.position.y < lookAt.position.y) ? deltaY - boundY : deltaX + boundY;
+
+        transform.position += new Vector3(deltaX, deltaY, 0);
+    }
+}
---
+++ TopDownRPG/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider; // player box collider
+
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Reset moveDelta;
+        moveDelta = Vector3.zero;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction based on horizontal direction
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Make player move in y direction.
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        
+        if (hit.collider == null)
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        // Make player move in y direction.
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if (hit.collider == null)
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+
+
+
+
+    }
+}
